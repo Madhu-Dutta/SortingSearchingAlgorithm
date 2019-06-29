28e279054f46df4fdb2da8a2fb62f2d813b084c8
Madhu-Dutta <mdutta787@gmail.com>
2019-06-30 09:23:45 +1000

    SecondCommit - Adding files

---
+++ SearchingSorting/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SearchingSorting;
+using System.Diagnostics;
+
+namespace SearchingSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TEST ON CSV UNSORTED DATA
+            //Reading the csv file
+            StreamReader sr = new StreamReader("../../unsorted_numbers.csv");
+            string data = sr.ReadToEnd();//.ReadLine();
+
+            string[] str = data.Split(new char[1] { '\n' });
+            int[] numArr = str.Select(c => Convert.ToInt32(c.ToString())).ToArray();
+
+            //TEST ON SMALL ARRAY
+            int[] testArrSmall = { 1, 22, 3, 56, 7, 23, 8, 24, 9, 25, 10, 26, 11, 27, 12, 28, 13, 837493, 89, 321 };
+            //Console.WriteLine("Original Array: " + string.Join(",", testArrSmall));
+            //Console.WriteLine("##############################################");
+            //Console.WriteLine();
+
+            //TEST ON LARGE ARRAY
+            int[] testArrLg = new int[100000000];
+            for (int i = 0; i < 100000000; i++)
+            {
+                testArrLg[i] = i;
+            }
+
+            //SORTING IMPLEMENTATIONS
+            SortingAlgorithms sort = new SortingAlgorithms();
+            //INSERTION SORT IMPLEMENTATION
+            int[] insertionsortedcsvdata;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            insertionsortedcsvdata = sort.DoInsertionSort(numArr);
+            stopwatch.Stop();
+
+            Console.WriteLine("*******************************************");
+
+            //Console.WriteLine("Insertion Sorted Array: " + string.Join(",", insertionsortedcsvdata));
+            Console.WriteLine("Insertion Sort took {0} ms" , stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Insertion Sort took {0} ticks", stopwatch.ElapsedTicks);
+
+            Console.WriteLine("*********************************************");
+           
+            //SHELL SORT IMPLEMENTATION
+            int[] shellsortedcsvdata;
+            stopwatch = Stopwatch.StartNew();
+            shellsortedcsvdata = sort.DoShellSort(numArr);
+            stopwatch.Stop();
+
+            //Console.WriteLine("Shell Sorted Array: " + string.Join(",", shellsortedcsvdata));
+            Console.WriteLine("Shell Sort took {0} ms", stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Shell Sort took {0} ticks", stopwatch.ElapsedTicks);
+            Console.WriteLine("*********************************************** ");
+
+
+            //MERGE SORT IMPLEMENTATION            
+
+            int[] mergesortedcsvdata;
+            stopwatch = Stopwatch.StartNew();
+            mergesortedcsvdata = sort.DoMergeSort(numArr, 0, numArr.Length-1);
+            //mergesortedcsvdata = sort.DoMergeSort(testArrSmall, 0, testArrSmall.Length - 1);            
+            stopwatch.Stop();
+
+            //Console.WriteLine("Merge Sorted Array: " + string.Join(",", mergesortedcsvdata));
+            Console.WriteLine("Merge Sort took {0} ms", stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Merge Sort took {0} ticks", stopwatch.ElapsedTicks);
+            Console.WriteLine("##############################################");
+
+            //SEARCHING IMPLEMENTATIONS
+            SearchingAlgorithms search = new SearchingAlgorithms(); 
+            
+            //Find the largest number in the sorted csv file using binary and linear search
+            int largestNum = shellsortedcsvdata[shellsortedcsvdata.Length - 1];
+
+            bool found = false;
+            stopwatch = Stopwatch.StartNew();
+            found = search.DoBinarySearch(shellsortedcsvdata, largestNum);
+            stopwatch.Stop();
+
+            //Console.WriteLine("The Item exits: " + found);
+            Console.WriteLine("Binary Search for the largest number: {0} took {1} ms", largestNum, stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Binary Search for the largest number: {0} took {1} ticks", largestNum, stopwatch.ElapsedTicks);
+
+            stopwatch = Stopwatch.StartNew();
+            found = search.DoLinearSearch(shellsortedcsvdata, largestNum);
+            stopwatch.Stop();
+
+            //Console.WriteLine("The Item exits: " + found);
+            Console.WriteLine("Linear Search for the largest number: {0} took {1} ms", largestNum, stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Linear Search for the largest number: {0} took {1} ticks", largestNum, stopwatch.ElapsedTicks);
+            Console.WriteLine("**************************************************************");
+
+            //Find the largest number in the testArrLg using binary and linear search
+           
+            largestNum = testArrLg[testArrLg.Length - 1];
+            
+            stopwatch = Stopwatch.StartNew();
+            found = search.DoBinarySearch(testArrLg, largestNum);
+            stopwatch.Stop();            
+            Console.WriteLine("Binary Search for the largest number: {0} took {1} ms", largestNum, stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Binary Search for the largest number: {0} took {1} ticks", largestNum, stopwatch.ElapsedTicks);
+
+            stopwatch = Stopwatch.StartNew();
+            found = search.DoLinearSearch(testArrLg, largestNum);
+            stopwatch.Stop();
+            Console.WriteLine("Linear Search for the largest number: {0} took {1} ms", largestNum, stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Linear Search for the largest number: {0} took {1} ticks", largestNum, stopwatch.ElapsedTicks);
+
+            //Find every 1500 element using Binary and Linear Search
+            for (int i = 0; i < shellsortedcsvdata.Length; i=i+1500)
+            {
+                stopwatch = Stopwatch.StartNew();
+                found = search.DoBinarySearch(shellsortedcsvdata, shellsortedcsvdata[i]);
+                stopwatch.Stop();
+                Console.WriteLine("Binary Search of {0} took {1} ms", i + "th number: " + shellsortedcsvdata[i], stopwatch.ElapsedMilliseconds);
+                Console.WriteLine("Binary Search of {0} took {1} ticks", i + "th number: " + shellsortedcsvdata[i], stopwatch.ElapsedTicks);
+
+                stopwatch = Stopwatch.StartNew();
+                found = search.DoLinearSearch(shellsortedcsvdata, shellsortedcsvdata[i]);
+                stopwatch.Stop();
+                Console.WriteLine("Linear Search of {0} took {1} ms", i + "th number: " + shellsortedcsvdata[i], stopwatch.ElapsedMilliseconds);
+                Console.WriteLine("Linear Search of {0} took {1} ticks", i + "th number: " + shellsortedcsvdata[i], stopwatch.ElapsedTicks);
+            }
+
+            Console.Read();
+        }
+        
+    }
+}
---
+++ SearchingSorting/SearchingSorting.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingSorting
+{
+  
+    public class SortingAlgorithms
+    {        
+        public int[] DoInsertionSort(int[] nums)
+        {
+            int[] arr = new int[nums.Length];
+            nums.CopyTo(arr,0);        
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                var indexMin = i;
+
+                for(var j = i + 1; j < arr.Length; j++)
+                {
+                    if(arr[indexMin] > arr[j])
+                    {
+                        indexMin = j;
+                    }
+                }
+                var temp = arr[indexMin];
+                arr[indexMin] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+        public int[] DoShellSort(int[] nums)
+        {
+            int[] arr = new int[nums.Length];
+            nums.CopyTo(arr, 0);
+
+            int gap = arr.Length / 2;
+
+            while(gap > 0)
+            {
+                int j = 0;
+                for(int i = gap; i < arr.Length; i++)
+                {
+                    int temp = arr[i]; 
+                    for(j = i; j >= gap && arr[j -gap] > temp; j -= gap)
+                    {
+                        arr[j] = arr[j - gap];
+                    }
+                    arr[j] = temp;
+                }
+                gap = gap / 2;
+            }
+            return arr;
+        }
+        public int[] DoMergeSort(int[] nums, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                DoMergeSort(nums, left, middle);
+                DoMergeSort(nums, middle + 1, right);
+
+                //Main Merge
+                int[] leftArray = new int[middle - left + 1];
+                int[] rightArray = new int[right - middle];
+
+                Array.Copy(nums, left, leftArray, 0, middle - left + 1);
+                Array.Copy(nums, middle + 1, rightArray, 0, right - middle);
+
+                int i = 0;
+                int j = 0;
+                for (int k = left; k < right + 1; k++)
+                {
+                    if (i == leftArray.Length)
+                    {
+                        nums[k] = rightArray[j];
+                        j++;
+                    }
+                    else if (j == rightArray.Length)
+                    {
+                        nums[k] = leftArray[i];
+                        i++;
+                    }
+                    else if (leftArray[i] <= rightArray[j])
+                    {
+                        nums[k] = leftArray[i];
+                        i++;
+                    }
+                    else
+                    {
+                        nums[k] = rightArray[j];
+                        j++;
+                    }
+                }
+            }
+            return nums;
+        }
+        
+    }
+
+    public class SearchingAlgorithms
+    {        
+        
+        public bool DoLinearSearch(int[] nums, int item)
+        {           
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == item)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public bool DoBinarySearch(int[] nums, int item)
+        {
+            int startpt = 0;
+            int endpt = nums.Length - 1;
+            int midpt = (startpt + endpt) / 2;
+            
+            while (midpt >= 0) {
+
+                midpt = (startpt + endpt) / 2;
+                
+                if (nums[midpt] < item)
+                {
+                    startpt = midpt + 1;
+
+                }
+                else if (nums[midpt] > item)
+                {
+                    endpt = midpt - 1;
+
+                }
+                else
+                {
+                    Console.WriteLine(nums[midpt]);
+                    return true;
+                }
+            }      
+            
+            return false;
+        }
+    }
+}
